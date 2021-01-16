40380df19513ff3b853cf57cdd9ac47e398241bb
Anna Brandenberger <anna.brandenberger@mail.mcgill.ca>
2021-01-16 01:19:00 -0500

    stuffffs

--- public/script.js
+++ public/script.js
@@ -1,12 +1,16 @@
-let userinput = document.getElementById('question');
+let input = document.getElementById('question');
 
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function (event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        document.getElementById("button").click();
-    }
-}); 
+function submit() {
+    // TODO WHATEVER NEEDS TO HAPPEN WHEN CLICKED 
+}
+
+// // Execute a function when the user releases a key on the keyboard
+// input.addEventListener("keyup", function (event) {
+//     // Number 13 is the "Enter" key on the keyboard
+//     if (event.keyCode === 13) {
+//         // Cancel the default action, if needed
+//         event.preventDefault();
+//         // Trigger the button element with a click
+//         document.getElementById("button").click();
+//     }
+// }); 
--- public/stars.js
+++ public/stars.js
@@ -80,54 +80,54 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 // if i change anything about camera properties must call
 // camera.updateProjectionMatrix()
 
-// window.addEventListener("load", init);
+window.addEventListener("load", init);
 
-// function init() {
-let rot = 0;
-const scene = new THREE.Scene();
-scene.fog = new THREE.Fog(0xaaaaaa, 50, 2000);
+function init() {
+    let rot = 0;
+    const scene = new THREE.Scene();
+    scene.fog = new THREE.Fog(0xaaaaaa, 50, 2000);
 
-const camera = new THREE.PerspectiveCamera(70, 1000);
-const geometry = new THREE.Geometry();
+    const camera = new THREE.PerspectiveCamera(70, 1000);
+    const geometry = new THREE.Geometry();
 
-for (let i = 0; i < 10000; i++) {
-    const star = new THREE.Vector3();
-    star.x = THREE.Math.randFloatSpread(2000);
-    star.y = THREE.Math.randFloatSpread(2000);
-    star.z = THREE.Math.randFloatSpread(2000);
+    for (let i = 0; i < 10000; i++) {
+        const star = new THREE.Vector3();
+        star.x = THREE.Math.randFloatSpread(2000);
+        star.y = THREE.Math.randFloatSpread(2000);
+        star.z = THREE.Math.randFloatSpread(2000);
 
-    geometry.vertices.push(star)
+        geometry.vertices.push(star)
+    }
+
+    const material = new THREE.PointsMaterial({
+        color: 0xffffff
+    });
+    const starField = new THREE.Points(geometry, material);
+    scene.add(starField);
+
+    function render() {
+        rot += 0.03;
+        const radian = (rot * Math.PI) / 180;
+        camera.position.x = 1000 * Math.sin(radian);
+        camera.position.z = 1000 * Math.cos(radian);
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(render);
+    }
+    render();
+
+    window.addEventListener("resize", onResize);
+
+    function onResize() {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(width, height);
+
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+    }
+    onResize();
 }
-
-const material = new THREE.PointsMaterial({
-    color: 0xffffff
-});
-const starField = new THREE.Points(geometry, material);
-scene.add(starField);
-
-function render() {
-    rot += 0.03;
-    const radian = (rot * Math.PI) / 180;
-    camera.position.x = 1000 * Math.sin(radian);
-    camera.position.z = 1000 * Math.cos(radian);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-    renderer.render(scene, camera);
-
-    requestAnimationFrame(render);
-}
-render();
-
-window.addEventListener("resize", onResize);
-
-function onResize() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(width, height);
-
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-}
-onResize();
-// }
